44611aa4da83b3c4f4cd4303c03df0a7cf8d4a13
ankd-k <ankd.2653@gmail.com>
2018-06-24 19:21:55 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# SetWindowResolution
+This package set window resolution by entering width and height number.
+
+# setting
+- width : init width in textbox.
+- height : init height in textbox.
---
+++ spec/set-window-resolution-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SetWindowResolutionView from '../lib/set-window-resolution-view';
+
+describe('SetWindowResolutionView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
